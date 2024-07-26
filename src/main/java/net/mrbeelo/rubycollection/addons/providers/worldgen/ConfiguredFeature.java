d7d89e8962f8fa71fa4a6197a7a131891377553b
MrBeelo <mrbeelomakris@gmail.com>
2024-07-26 15:25:08 +0300

    Fixed ore gen

--- src/main/java/net/mrbeelo/rubycollection/addons/providers/worldgen/ConfiguredFeature.java
+++ src/main/java/net/mrbeelo/rubycollection/addons/providers/worldgen/ConfiguredFeature.java
@@ -41,9 +41,9 @@ public class ConfiguredFeature {
         List<OreFeatureConfig.Target> endRubyTargets = List.of(
                 OreFeatureConfig.createTarget(endOreReplaceables, ModBlocks.END_RUBY_ORE.getDefaultState()));
 
-        register(context, OVERWORLD_RUBY_ORE_KEY, Feature.ORE, new OreFeatureConfig(overworldRubyTargets, 5, 0.5F));
-        register(context, NETHER_RUBY_ORE_KEY, Feature.ORE, new OreFeatureConfig(netherRubyTargets, 5, 0.2F));
-        register(context, END_RUBY_ORE_KEY, Feature.ORE, new OreFeatureConfig(endRubyTargets, 2));
+        register(context, OVERWORLD_RUBY_ORE_KEY, Feature.ORE, new OreFeatureConfig(overworldRubyTargets, 5, 0.3F));
+        register(context, NETHER_RUBY_ORE_KEY, Feature.ORE, new OreFeatureConfig(netherRubyTargets, 5, 0.3F));
+        register(context, END_RUBY_ORE_KEY, Feature.ORE, new OreFeatureConfig(endRubyTargets, 2, 0.3F));
 
         //METHODS
 
